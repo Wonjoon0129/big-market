6c2c55ec1ea8133544beb5c0e53d497cdef34572
wonjoon <153899070+Wonjoon0129@users.noreply.github.com>
2025-08-03 18:32:32 +0800

    分布式动态配置活动降级

---
+++ big-market-api/src/main/java/org/example/trigger/api/IDCCService.java
@@ -0,0 +1,15 @@
+package org.example.trigger.api;
+
+
+import org.example.trigger.api.response.Response;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description DCC 动态配置中心
+ * @create 2024-07-13 08:58
+ */
+public interface IDCCService {
+
+    Response<Boolean> updateConfig(String key, String value);
+
+}
